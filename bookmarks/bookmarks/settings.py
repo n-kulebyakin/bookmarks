edc98e0dcbd2ebace9ad361da6100ae14a783783
n-kulebyakin <nakulebyakin@yandex.ru>
2021-10-05 09:47:40 +0300

    thumbnail added

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'social_django',
     'images.apps.ImagesConfig',
+    'sorl.thumbnail',
 ]
 
 MIDDLEWARE = [
---
+++ bookmarks/images/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+app_name = 'images'
+
+urlpatterns = [
+    path('create/', views.image_create, name='create'),
+]
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    paht('account/', include('accounts.urls')),
+    paht('social-auth/',
+         include('social_django.urls', namespace='social')),
+    path('images/', include('images.urls', namespace='images')),
+    path('detail/<int:id>/<slug:slug>/', views.image_detail, name='detail'),
+]
