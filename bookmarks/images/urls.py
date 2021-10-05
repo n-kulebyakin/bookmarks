7321c89f6181efab3f33c394471cba76be667924
n-kulebyakin <nakulebyakin@yandex.ru>
2021-10-05 10:47:18 +0300

    ajax added

--- bookmarks/images/urls.py
+++ bookmarks/images/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
          include('social_django.urls', namespace='social')),
     path('images/', include('images.urls', namespace='images')),
     path('detail/<int:id>/<slug:slug>/', views.image_detail, name='detail'),
+    path('like/', views.image_like, name='like'),
 ]
